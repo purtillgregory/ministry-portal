c9c44725c40ebc45c8dc2fb09c1da4bee4b52044
purtillgregory <purtillg@comcast.net>
2025-06-15 16:54:07 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,14 @@
 // js/firebase-config.js
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  // ...any others like storageBucket, messagingSenderId, appId
+  apiKey: "AIzaSyDOA3sQsHk91zddql2PjpCSTFLH_H7_ITI",
+  authDomain: "mts-portal-35024.firebaseapp.com",
+  projectId: "mts-portal-35024",
+  storageBucket: "mts-portal-35024.firebasestorage.app",
+  messagingSenderId: "784836501245",
+  appId: "1:784836501245:web:db000d99f3a6525e192894",
+  measurementId: "G-LJH998H4GT"
 };
 
+// ✅ Initialize Firebase
 firebase.initializeApp(firebaseConfig);
