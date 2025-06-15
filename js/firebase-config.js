aab680760f5ad3a6231805550f1b7bacd4ca09ce
purtillgregory <purtillg@comcast.net>
2025-06-15 15:57:23 -0400

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+// js/firebase-config.js
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  // ...any others like storageBucket, messagingSenderId, appId
+};
+
+firebase.initializeApp(firebaseConfig);
